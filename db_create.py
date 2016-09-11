fea527ef3751978cdd85a924b5d4163dc51f80e3
GuoL <guocl85@163.com>
2016-09-11 10:28:57 +0800

    添加数据库初始化到db_create.py

--- db_create.py
+++ db_create.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
 
+import os.path
+
 from migrate.versioning import api
+
 from config import SQLALCHEMY_DATABASE_URI
 from config import SQLALCHEMY_MIGRATE_REPO
 from app import db
-import os.path
+from app import users
+from app import common
+
 db.create_all()
+
 if not os.path.exists(SQLALCHEMY_MIGRATE_REPO):
     api.create(SQLALCHEMY_MIGRATE_REPO, 'database repository')
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
 else:
     api.version_control(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO, api.version(SQLALCHEMY_MIGRATE_REPO))
+
+users.models.Role.insert_roles()
+a = common.models.SystemSettings()
+a.title = 'OpenStack'
+a.save()
